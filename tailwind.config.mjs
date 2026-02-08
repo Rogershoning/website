373c04aeba1bb982d03a2512330c0be1d36db44a
Rogershoning <roger@rogershoning.nl>
2026-02-08 12:31:09 +0100

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'honing-goud': '#FFB400',
+        'antraciet': '#2D2D2D',
+        'creme': '#FAF9F6',
+      },
+    },
+  },
+  plugins: [],
+}
