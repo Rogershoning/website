a327cc57ff18ccac50fdae9a1467c932c77c2cbf
Rogershoning <roger@rogershoning.nl>
2026-02-08 11:14:27 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  // De 'snufjes' (View Transitions) zijn nu standaard, 
+  // dus we hebben geen experimental vlag meer nodig.
   integrations: [tailwind()],
-  // Voor de nieuwste 'internet snufjes'
-  experimental: {
-    viewTransitions: true
-  }
 });
